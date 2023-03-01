771d9001d8d6a41c7da53ec24352b0dda87d89f3
JKFutureTech <125233762+JKFutureTech@users.noreply.github.com>
2023-03-01 09:41:02 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let lastPrompt = '';
+
 app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Hello from AI Joel',
@@ -27,13 +29,15 @@ app.post('/', async (req, res) => {
 
         const response = await openai.createCompletion({
             model:"text-davinci-003",
-            prompt:`${prompt}`,
+            prompt:`${lastPrompt} ${prompt}`,
             temperature:0,
             max_tokens:3000,
             top_p:1,
             frequency_penalty:0.5,
             presence_penalty:0,
         });
+
+        lastPrompt = prompt;
 
         res.status(200).send({
             bot: response.data.choices[0].text
