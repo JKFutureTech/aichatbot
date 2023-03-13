1b8ccf60c4264bb03c9b8478c4e1a31e43f240be
JKFutureTech <125233762+JKFutureTech@users.noreply.github.com>
2023-03-13 18:07:48 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,11 +30,7 @@ app.post('/', async (req, res) => {
 
         const response = await openai.createCompletion({
              model:"gpt-3.5-turbo",
-            messages: [
-                            {"role": "system", "content": "your name is AI Joel and you are a helpful assistant."},
-                            {"role": "user", "content": "Who won the world series in 2020?"},
-                            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                            {"role": "user", "content": $prompt}],                             
+            messages: [{"role": "user", "content": $prompt}],                             
             temperature:0,
             max_tokens:4000,
             top_p:1,
