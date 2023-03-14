688563cc3197a77a0867714524b8739c42a949c3
JKFutureTech <125233762+JKFutureTech@users.noreply.github.com>
2023-03-13 22:59:42 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.post('/', async (req, res) => {
         });
 
         res.status(200).send({
-        bot: response.data.choices[0].message.content
+        bot: response.data.choices[0].text
         })        
 
   
