74c6ff6dbba511074ba9c110267215bf6a911a0d
JKFutureTech <125233762+JKFutureTech@users.noreply.github.com>
2023-03-13 23:23:36 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,21 +27,20 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
-        
-    
-        
-       const messages = [{ "role": "user", "content": prompt }];
 
-
-        
-      const response = await openai.createCompletion({
-            model:"gpt-3.5-turbo", 
-            messages, 
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt:`${prompt}`,
+            temperature:0,
+            max_tokens:3000,
+            top_p:1,
+            frequency_penalty:0.5,
+            presence_penalty:0,
         });
 
         res.status(200).send({
-        bot: response.data.choices[0].message.content
-        })        
+            bot: response.data.choices[0].text
+        })       
 
   
     } catch (error) {
