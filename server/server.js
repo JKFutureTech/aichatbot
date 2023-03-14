fe50dacea1c4c11ca807e3caf0be99f94232a8ac
JKFutureTech <125233762+JKFutureTech@users.noreply.github.com>
2023-03-13 21:41:30 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,8 +40,8 @@ app.post('/', async (req, res) => {
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": "Who won the world series in 2020?"},
                 {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                {"role": "user", "content": `$prompt`}
-            ]
+                {"role": "user", "content": `prompt`}
+            ],
         });
 
         res.status(200).send({
